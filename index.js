df538d76f88d13529a1898dc33809e5f045c16ea
Gr8Tortuga <jasonkiser@gmail.com>
2017-10-04 01:42:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if whisper(string) ===  string {
+  if (string ===  whisper(string)) {
     return ("I can\'t hear you!")
   }
